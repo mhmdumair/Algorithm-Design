7be085c646f539e85345286ff9c01bc60a1ee14d
Umair <umairzahiraj16@gmail.com>
2025-04-05 16:32:43 +0530

    Buble sort algorithm is complete

--- BubbleSort.py
+++ BubbleSort.py
@@ -3,11 +3,13 @@ arr = [42,35,12,77,54,101,5]
 def bubblrSort(arr):
     n = len(arr)
     for j in range(n-1):
+
         swapped = False
         for i in range(n-j-1):
             if (arr[i] > arr[i+1]):
                 arr[i], arr[i+1] = arr[i+1], arr[i]
                 swapped = True
+
         if not swapped:
             return
 
